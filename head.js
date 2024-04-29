122257f895aad8761bbc452f8390dbf093a77d4e
AEgor18 <147916286+AEgor18@users.noreply.github.com>
2024-04-29 14:11:09 +0300

    Add files via upload

---
+++ head.js
@@ -0,0 +1,229 @@
+document.getElementById('heart-btn1').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon1').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon1').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon1').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn2').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon2').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon2').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon2').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn3').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon3').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon3').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon3').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn4').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon4').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon4').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon4').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn5').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon5').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon5').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon5').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn6').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon6').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon6').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon6').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn7').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon7').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon7').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon7').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn8').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon8').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon8').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon8').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn9').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon9').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon9').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon9').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn10').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon10').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon10').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon10').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn11').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon11').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon11').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon11').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn12').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon12').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon12').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon12').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn13').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon13').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon13').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon13').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn14').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon14').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon14').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon14').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn15').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon15').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon15').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon15').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn16').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon16').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon16').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon16').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn17').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon17').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon17').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon17').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn18').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon18').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon18').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon18').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn19').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon19').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon19').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon19').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+document.getElementById('heart-btn20').addEventListener('click', function() {
+  var imgSrc = document.getElementById('heart-icon20').getAttribute('src');
+  
+  if(imgSrc === "/фото/Heart1.svg") {
+      document.getElementById('heart-icon20').setAttribute('src', '/фото/icons8-сердце-30.png' );
+  } else {
+      document.getElementById('heart-icon20').setAttribute('src', "/фото/Heart1.svg");
+  }
+})
+
+const button = document.querySelector('.buttons-btn1')
+const image = document.querySelector('.buttons-img1')
+
+button.addEventListener('mouseenter', function() {
+    image.src = '/фото/arrwhiteleft.png'
+    image.style.backgroundColor = '#2DB224'
+    button.style.backgroundColor = '#2DB224'
+})
+
+button.addEventListener('mouseleave', function() {
+    image.src = "/фото/arrgreenleft.png"
+    image.style.backgroundColor = '#fff'
+    button.style.backgroundColor = '#fff'
+})
+
+const button1 = document.querySelector('.buttons-btn3')
+const image1 = document.querySelector('.buttons-img2')
+
+button1.addEventListener('mouseenter', function() {
+    image1.src = "/фото/arrwhiterightpng.png"
+    image1.style.backgroundColor = '#2DB224'
+    button1.style.backgroundColor = '#2DB224'
+})
+
+button1.addEventListener('mouseleave', function() {
+    image1.src = "/фото/arrgreenright.png"
+    image1.style.backgroundColor = '#fff'
+    button1.style.backgroundColor = '#fff'
+})
