bfcea5ee01c4c9a97a9fdc4ae5983f576b757774
AEgor18 <147916286+AEgor18@users.noreply.github.com>
2024-05-16 09:10:25 +0300

    Add files via upload

---
+++ js/pay.js
@@ -0,0 +1,64 @@
+document.querySelector('.header-line3-new').addEventListener('click', function() {
+    var category = document.querySelector('.category');
+    if (category.style.display === 'none' || category.style.display === '') {
+        category.style.display = 'block';
+    } else {
+        category.style.display = 'none';
+    }
+});
+
+const cardNumberInput = document.getElementById('cardNumber');
+
+function formatCardNumber(event) {
+    const currentValue = event.target.value;
+    
+    if (currentValue.length > 19) {
+    
+        event.target.value = currentValue.slice(0, 19);
+    } else {
+        
+        let formattedValue = currentValue.replace(/(\d{4})(?=\d)/g, '$1 ');
+        
+        event.target.value = formattedValue;
+    }
+}
+
+cardNumberInput.addEventListener('input', formatCardNumber);
+
+const cardDataInput = document.getElementById('cardData');
+
+function formatCardData(event) {
+    const currentValue = event.target.value;
+    
+    if (currentValue.length > 5) {
+    
+        event.target.value = currentValue.slice(0, 5);
+    } else {
+        
+        let formattedValue = currentValue.replace(/(\d{2})(?=\d)/g, '$1/');
+        
+        event.target.value = formattedValue;
+    }
+}
+cardDataInput.addEventListener('input', formatCardData);
+
+const cardCvvInput = document.getElementById('cardCvv');
+
+function formatCardCvv(event) {
+    const currentValue = event.target.value;
+    
+    if (currentValue.length > 3) {
+    
+        event.target.value = currentValue.slice(0, 3);
+}
+}
+
+cardCvvInput.addEventListener('input', formatCardCvv);
+
+
+
+
+
+
+
+
